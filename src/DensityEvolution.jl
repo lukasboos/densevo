f998b37a6f869aa57aec62a0fb7025a13692dc81
Lukas B <lboos@gmx.de>
2022-08-05 15:18:26 +0200

    update

--- src/DensityEvolution.jl
+++ src/DensityEvolution.jl
@@ -171,17 +171,21 @@ end
 ############################################################################################
 # Calculate probability distribution
 ############################################################################################
+
 function todistribution()
+    LLR_cn_1 = 0.0
+    LLR_cn_sum = 0.0
+    p_cn = 0.0
     # LLR sum for 1
     for i in n/2:n
-        LLR_ext_cn_1 += LLR_ext_cn[i]
+        LLR_cn_1 += LLRs[i]
     end
     # LLRs sum total
     for i in 1:n
-        LLR_ext_cn_sum += LLR_ext_cn[i]
+        LLR_cn_sum += LLR_ext_cn[i]
     end
     # calculate probability distribution
-    p_cn = LLR_ext_cn_0/LLR_ext_cn_sum
+    p_cn = LLR_cn_0/LLR_cn_sum
 end
 todistribution()
 

4e259aa682ea247ceee2f0fc94253f6f5e35432f
Lukas B <lboos@gmx.de>
2022-08-26 09:09:56 +0200

    Update

--- src/DensityEvolution.jl
+++ src/DensityEvolution.jl
@@ -28,14 +28,15 @@ cdf(x, σ, μ) = 1 / 2 * (1 + erf((x - μ) / (σ * sqrt(2))))
 # Equation (2.50)
 LLR(σ, μ) = log((1 - cdf(0, σ, μ)) / cdf(0, σ, μ))
 
-
-function box_plus(a::Vector)
-    result = 0.0
-    for i in 1:length(a)-1
-        dividend = 1 + exp(a[i] + a[i+1])
-        divisor = exp(a[i]) + exp(a[i+1])
-        result = log(dividend / divisor)
+#looks like result is similar to a multiplication
+function box_plus(a::Vector, b::Vector, n)
+    result = ones(Float64, n)
+    for i in 1:n
+        dividend = 1 + exp(a[i] + b[i])
+        divisor = exp(a[i]) + exp(b[i])
+        result[i] = log(dividend / divisor)
     end
+    @show length(result)
     return result
 end
 
@@ -133,48 +134,83 @@ n = 2000
 x = -10:0.01:35
 
 # Sample vector LLRs per check node since every check node contains same LLRs a vector is sufficient. Otherwise we need one per cn
-LLRs = Vector{Float64}(undef, n)
+samples = ones(Float64, n) #Vector{Float64}(undef, n)
 # Extrinsic LLRs per check node since every check node contains same LLRs a vector is sufficient. Otherwise we need one per cn
-LLR_ext_cn = Vector{Float64}(undef, n)
+LLR_ext_cn = ones(Float64, n, d_c)#Matrix{Float64}(undef, n, d_c)
 
 ############################################################################################
 # Sample Normal distribution
 ############################################################################################
 
 # Calculate sample Values for each check node
-function sample(x)
+function sample(x, n, σ, μ)
     # Calculate step size
     steps = (last(x)-x[1])/n
     x_n = x[1]
     for i in 1:n
-        LLRs[i] = f(x_n, σ, μ)
+        samples[i] = f(x_n, σ, μ)
         x_n += steps
     end
-    return LLRs
+    return samples
 end
-sample(x)
+samples = sample(x, n, σ, μ)
+@show length(samples)
+
+# TODO: plot function of samples
+# Plot function (divided by 6 for normalization)
+y = samples ./ d_c
+x=1:2000
+#y=rand(10)
+
+#plot(x, y)
+
+plot(x, y, title = "Plotted Samples")
+savefig("myplot.png")
+
 
 ############################################################################################
 # Calculate check node LLRs by boxplus operation
 ############################################################################################
 # init
-LLR_ext_cn[1] = LLRs[1]
+#LLR_ext_cn[1] = samples[1]
 
 # calculate LLR_ext_cn for d_c checknodes
-for edge_out in 1:d_c
-    for edge_in in 1:d_c
-        if edge_out==edge_in
-            continue
-        else
-            LLR_ext_cn[edge_out] = box_plus(LLRs) * d_c-1
+function cn_init(d_c, a::Vector, n)
+    result = Matrix{Float64}(undef, n, d_c)
+    for edge_out in 1:d_c
+        first = true
+        for edge_in in 1:d_c
+            if edge_out==edge_in
+                continue
+            else
+                if first
+                    result[:, edge_out] = a
+                    first = false
+                    #@show result[:, 1]
+                else
+                    result[:, edge_out] = box_plus(a, result[:, edge_out], n)# .* (d_c-1)
+            end
         end
-    end
+    end 
+    return result
 end 
+LLR_ext_cn = cn_init(d_c, samples, n)
+
+# Plot LLR_ext_cn (divided by 6 for normalization)
+y = LLR_ext_cn[:, 1] ./ d_c
+x=1:2000
+#y=rand(10)
+
+#plot(x, y)
+
+plot(x, y, title = "Plotted LLR_ext_cn")
+savefig("LLR_ext_plot.png")
 
 ############################################################################################
-# Calculate probability distribution
+# Calculate probability distribution see formula 2.50 (log of left part divided by right part)
 ############################################################################################
 
+#=
 function prob()
     LLR_cn_1 = 0.0
     LLR_cn_sum = 0.0
@@ -191,8 +227,8 @@ function prob()
     p_cn = LLR_cn_1/LLR_cn_sum
     @show p_cn
 end
-prob()
-
+LLR_cn = prob()
+=#
 ############################################################################################
 # Show initial results
 ############################################################################################
@@ -200,7 +236,7 @@ prob()
 @show LLR(σ, μ)
 
 # Plot function (divided by 6 for normalization)
-y(x) = f(x, σ, μ) / d_c
+#y(x) = f(x, σ, μ) / d_c
 
 plot(x, y)
 
@@ -273,3 +309,33 @@ function de(λ_d_c::Vector{Float64}, λ_d_v::Vector{Float64}, μ::AbstractFloat,
 end
 
 de(λ_d_c, λ_d_v, μ, σ)
+
+
+
+
+
+
+
+################################################
+c = ones(Float64, 8, 4)
+c0 = [-4.49, -4.49, -4.49, -4.49]
+c1 = [0.00, 0.00, 0.00, 0.00]
+c2 = [-4.49, -4.49, -4.49, -4.49]
+c3 = [-4.49, -4.49, -4.49, -4.49]
+c4 = [4.49, 4.49, 4.49, 4.49]
+c5 = [4.49, 4.49, 4.49, 4.49]
+c6 = [-4.49, -4.49, -4.49, -4.49]
+c7 = [-4.49, -4.49, -4.49, -4.49]
+
+c[1, :] = c0
+c[2, :] = c1
+c[3, :] = c2
+c[4, :] = c3
+c[5, :] = c4
+c[6, :] = c5
+c[7, :] = c6
+c[8, :] = c7
+
+@show(c)
+
+L = box_plus(c0, c2, 4)
